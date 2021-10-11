6cb251ec88040127e248f884d0a201276464b843
dyuguytjdr <74043933+dyuguytjdr@users.noreply.github.com>
2021-10-11 15:58:51 +0300

    1-4 работы

---
+++ WPF1/War6/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace War7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            double a = rand.Next(10, 100);
+            label.Content = "Случайно сгененрированное число: " + a;
+            label1.Content = "Сумма числе: " + Math.Truncate(a % 10 + a/10);
+            label2.Content = "Произведение чисел: " + a % 10 * Math.Truncate(a / 10);
+        }
+    }
+}
---
+++ WPF4/War1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace War1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Window wind1 = new Window1();
+            wind1.Show();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Window wind2 = new Window2();
+            wind2.Show();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window wind3 = new Window3();
+            wind3.Show();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Window wind4 = new Window4();
+            wind4.Show();
+        }
+    }
+}
